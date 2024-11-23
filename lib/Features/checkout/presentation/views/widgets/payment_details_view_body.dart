39a10da74d517332ce6b9aafce1968bef998efc0
Abdelrhman Nashaat <abdelrhman.nashaat22@gmail.com>
2024-11-23 14:07:02 +0200

    first commit

---
+++ lib/Features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -0,0 +1,63 @@
+import 'package:checkout_payment_ui/Features/checkout/presentation/views/widgets/payment_methods_list_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+class PaymentDetailsViewBody extends StatelessWidget {
+  const PaymentDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          PaymentMethodsListView(),
+          CustomCreditCard(),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomCreditCard extends StatefulWidget {
+  const CustomCreditCard({super.key});
+
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+  String cardNumber = '', expiryDate = '', cardHolderName = '', cvvCode = '';
+
+  bool showBackView = false;
+  final GlobalKey<FormState> formKey = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          showBackView: showBackView,
+          isHolderNameVisible: true,
+          onCreditCardWidgetChange: (value) {},
+        ),
+        CreditCardForm(
+            cardNumber: cardNumber,
+            expiryDate: expiryDate,
+            cardHolderName: cardHolderName,
+            cvvCode: cvvCode,
+            onCreditCardModelChange: (carditCardModel) {
+              cardHolderName = carditCardModel.cardHolderName;
+              expiryDate = carditCardModel.expiryDate;
+              cvvCode = carditCardModel.cvvCode;
+              cardNumber = carditCardModel.cardNumber;
+              showBackView = carditCardModel.isCvvFocused;
+              setState(() {});
+            },
+            formKey: formKey),
+      ],
+    );
+  }
+}
